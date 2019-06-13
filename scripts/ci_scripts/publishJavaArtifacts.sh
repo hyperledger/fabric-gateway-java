80fc84f904a5797cfc1642b2bf7720363651d1d3
andrew-coleman <andrew_coleman@uk.ibm.com>
2019-06-13 11:24:53 +0100

    FGJ-21 don’t publish javadoc jar
    Change-Id: Ibee8561b1107fd6dd5a3679338f2f43494fc3b6f
    Signed-off-by: andrew-coleman <andrew_coleman@uk.ibm.com>

--- scripts/ci_scripts/publishJavaArtifacts.sh
+++ scripts/ci_scripts/publishJavaArtifacts.sh
@@ -15,22 +15,20 @@ version=$(mvn -q \
 
 if [ $version_check -gt 0 ]; then
     # Publish gateway-java jar files to nexus
-    for artifacts in $version $version-javadoc; do
-        echo "Pushing fabric-gateway-java-$artifacts.jar to Nexus.."
-        mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
-          -DupdateReleaseInfo=true \
-          -Dfile=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$artifacts.jar \
-          -DrepositoryId=hyperledger-snapshots \
-          -Durl=https://nexus.hyperledger.org/content/repositories/snapshots/ \
-          -DgroupId=org.hyperledger.fabric-gateway-java \
-          -Dversion=$version \
-          -DartifactId=fabric-gateway-java \
-          -DgeneratePom=true \
-          -DuniqueVersion=false \
-          -Dpackaging=jar \
-          -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
-          echo "========> DONE <======="
-    done
+    echo "Pushing fabric-gateway-java-$version.jar to Nexus.."
+    mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
+      -DupdateReleaseInfo=true \
+      -Dfile=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version.jar \
+      -DrepositoryId=hyperledger-snapshots \
+      -Durl=https://nexus.hyperledger.org/content/repositories/snapshots/ \
+      -DgroupId=org.hyperledger.fabric-gateway-java \
+      -Dversion=$version \
+      -DartifactId=fabric-gateway-java \
+      -DgeneratePom=true \
+      -DuniqueVersion=false \
+      -Dpackaging=jar \
+      -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
+      echo "========> DONE <======="
 else
     # Publish gateway-java jar files to nexus
     echo "Pushing fabric-gateway-java-$version-SNAPSHOT.jar to Nexus releases.."
