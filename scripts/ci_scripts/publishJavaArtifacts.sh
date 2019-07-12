77aac5a1c5d0de155308193f3961a9587bf0cff2
Mark S. Lewis <mark_lewis@uk.ibm.com>
2019-07-12 13:59:50 +0100

    FGJ-30: Publish to Maven with correct group ID
    Change-Id: Ica2d49f41c7f8dc7cb3719ac369a9aa45070897f
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- scripts/ci_scripts/publishJavaArtifacts.sh
+++ scripts/ci_scripts/publishJavaArtifacts.sh
@@ -5,46 +5,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Get the version from pom.xml
-cd $WORKSPACE/$BASE_DIR
-version_check=$(cat pom.xml | grep "version" | grep -c "SNAPSHOT")
+cd "${WORKSPACE}/${BASE_DIR}"
 version=$(mvn -q \
     -Dexec.executable=echo \
     -Dexec.args='${project.version}' \
     --non-recursive \
     exec:exec)
 
-if [ $version_check -gt 0 ]; then
-    # Publish gateway-java jar files to nexus
-    echo "Pushing fabric-gateway-java-$version.jar to Nexus.."
+if [[ ${version} =~ SNAPSHOT ]]; then
+    echo "Pushing fabric-gateway-java-${version}.jar to Nexus snapshots"
     mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
       -DupdateReleaseInfo=true \
-      -Dfile=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version.jar \
-      -Djavadoc=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version-javadoc.jar \
-      -DpomFile=$WORKSPACE/$BASE_DIR/pom.xml \
+      -Dfile="${WORKSPACE}/${BASE_DIR}/target/fabric-gateway-java-${version}.jar" \
+      -Djavadoc="${WORKSPACE}/${BASE_DIR}/target/fabric-gateway-java-${version}-javadoc.jar" \
+      -DpomFile="${WORKSPACE}/${BASE_DIR}/pom.xml" \
       -DrepositoryId=hyperledger-snapshots \
       -Durl=https://nexus.hyperledger.org/content/repositories/snapshots/ \
-      -DgroupId=org.hyperledger.fabric-gateway-java \
-      -Dversion=$version \
-      -DartifactId=fabric-gateway-java \
       -DuniqueVersion=false \
-      -Dpackaging=jar \
-      -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
-      echo "========> DONE <======="
+      -gs "${GLOBAL_SETTINGS_FILE}" -s "${SETTINGS_FILE}"
+      echo '========> DONE <======='
 else
-    # Publish gateway-java jar files to nexus
-    echo "Pushing fabric-gateway-java-$version-SNAPSHOT.jar to Nexus releases.."
+    echo "Pushing fabric-gateway-java-${version}.jar to Nexus releases"
     mvn org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
       -DupdateReleaseInfo=true \
-      -Dfile=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version.jar \
-      -Djavadoc=$WORKSPACE/$BASE_DIR/target/fabric-gateway-java-$version-javadoc.jar \
-      -DpomFile=$WORKSPACE/$BASE_DIR/pom.xml \
+      -Dfile="${WORKSPACE}/${BASE_DIR}/target/fabric-gateway-java-${version}.jar" \
+      -Djavadoc="${WORKSPACE}/${BASE_DIR}/target/fabric-gateway-java-${version}-javadoc.jar" \
+      -DpomFile="${WORKSPACE}/${BASE_DIR}/pom.xml" \
       -DrepositoryId=hyperledger-releases \
       -Durl=https://nexus.hyperledger.org/content/repositories/releases/ \
-      -DgroupId=org.hyperledger.fabric-gateway-java \
-      -Dversion= $version\
-      -DartifactId=fabric-gateway-java \
       -DuniqueVersion=false \
-      -Dpackaging=jar \
-      -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
-      echo "========> DONE <======="
+      -gs "${GLOBAL_SETTINGS_FILE}" -s "${SETTINGS_FILE}"
+      echo '========> DONE <======='
 fi
