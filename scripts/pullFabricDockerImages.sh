6a6135afa01ca966389ad13ef16ebe2d3ff2db78
Brett Logan <brett.t.logan@ibm.com>
2019-12-20 11:47:41 +0300

    [FABCI-482] Update Nexus URL's to Artifactory
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- scripts/pullFabricDockerImages.sh
+++ scripts/pullFabricDockerImages.sh
@@ -1,10 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
-set -e -o pipefail
+set -euo pipefail
 
-# Pull images from Hyperledger Nexus and re-tag to latest
+# Pull images from Hyperledger Artifactory and re-tag to latest
 for IMAGE in peer orderer ca baseos ccenv javaenv nodeenv tools; do
-    docker pull "nexus3.hyperledger.org:10001/hyperledger/fabric-${IMAGE}:amd64-2.0.0-stable"
-    docker tag "nexus3.hyperledger.org:10001/hyperledger/fabric-${IMAGE}:amd64-2.0.0-stable" "hyperledger/fabric-${IMAGE}:amd64-latest"
-    docker tag "nexus3.hyperledger.org:10001/hyperledger/fabric-${IMAGE}:amd64-2.0.0-stable" "hyperledger/fabric-${IMAGE}"
+    docker pull "hyperledger-fabric.jfrog.io/fabric-${IMAGE}:amd64-2.0.0-stable"
+    docker tag "hyperledger-fabric.jfrog.io/fabric-${IMAGE}:amd64-2.0.0-stable" "hyperledger/fabric-${IMAGE}:amd64-latest"
+    docker tag "hyperledger-fabric.jfrog.io/fabric-${IMAGE}:amd64-2.0.0-stable" "hyperledger/fabric-${IMAGE}"
 done
