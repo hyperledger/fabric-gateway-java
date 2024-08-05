58592a3a6b014317ea77096ab9f74350b2f36200
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2024-08-05 12:14:04 +0200

    Use `docker compose` instead of `docker-compose`
    The old `docker-compose` command no longer exists in some environments
    and causes build failures. Instead use the replacement `docker compose`
    command.
    
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

--- src/test/fixtures/generate.sh
+++ src/test/fixtures/generate.sh
@@ -10,7 +10,7 @@ rm -rf crypto-material/crypto-config
 
 cd docker-compose
 
-docker-compose -f docker-compose-cli.yaml up -d
+docker compose -f docker-compose-cli.yaml up -d
 docker exec -t cli cryptogen generate --config=/etc/hyperledger/config/crypto-config.yaml --output /etc/hyperledger/config/crypto-config
 docker exec -t cli configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock /etc/hyperledger/config/genesis.block -channelID testchainid
 docker exec -t cli configtxgen -profile ThreeOrgsChannel -outputCreateChannelTx /etc/hyperledger/config/channel.tx -channelID mychannel
@@ -18,4 +18,4 @@ docker exec -t cli configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdat
 docker exec -t cli configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdate /etc/hyperledger/config/Org2MSPanchors.tx -channelID mychannel -asOrg Org2MSP
 docker exec -t cli cp /etc/hyperledger/fabric/core.yaml /etc/hyperledger/config
 docker exec -t cli sh /etc/hyperledger/config/rename_sk.sh
-docker-compose -f docker-compose-cli.yaml down --volumes
+docker compose -f docker-compose-cli.yaml down --volumes
